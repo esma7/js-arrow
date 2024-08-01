b7214a19cbf7ada96cf2ec29a6f59cd9f42e4026
esma3 <semababayeva9898@gmail.com>
2024-08-01 21:27:45 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+
+
+//Task 1 // arrow functiondan isdifade ederek ele bir function itifade edinki parametr olaraq array ve silinecek datani
+// gonderdiyinizde o function hemen datani silsin ve consolede silindiyi barede melumat cixsin eger data yoxdursa
+// oda ekranda cap olunsunki  data movcud deyil. meselen deleteFunc(arrayy,"Ali")
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// const array = ["Eli", "Veli", "Sabir", "Mirze", "Nubar", "Aygul", "Mezahir"];
+
+// let userData = 0;
+
+// const deleteFunc = (item, delUser) => {
+//     for (let index in item){
+//         if(item[index] == delUser){
+//             console.log(item.splice(index, 1));
+//             userData = 1;
+//             break;
+//         }       
+//     }
+
+//     if(userData == 0){
+//         alert("data movcud deyil");
+//     }
+
+//     console.log(item);
+// }
+
+// deleteFunc(array," ");
+
+
+
+
+
+
+
+//Task2 //  bunun eynisini elave etmek ucunde isdifade edin yani arrow functiondan isdifade ederek array ve yeni deyeri
+// parametr olaraq vererek arraya elave olunsun ve  elave olunanda logda elave oldugu barede melumat cixsin
+// arraya data elave edende eyni elementin tekrar elave olundugunda logda bu datanin artiq var oldugunu  bildirsin
+// meselen addedFunc(arrayy,10)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+const array = ["Eli", "Veli", "Sabir", "Mirze", "Nubar", "Aygul", "Mezahir"];
+
+let userName = 0;
+
+
+const userAdd = (item, addData) =>{
+    for(let index in item){
+        if(addData == " " || addData == "null"){
+            console.log("Data bosdur");
+            break;
+        } else if (item[index] == addData) {
+            userName = 1;           
+        } 
+      
+    }
+
+    if(userName == 0){
+        if(addData != "null" && addData != " "){
+            array.push(addData)
+            console.log(`${addData} ugurla elave olundu`);
+        }       
+        
+    } 
+    else{
+        console.log(`${addData} artiq movcuddur`);
+    }
+    
+    console.log(item);
+}
+
+userAdd(array,"Eli");
+
+
+
+
+
+
+
+
+
+
